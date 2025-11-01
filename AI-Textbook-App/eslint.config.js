b0071f5fdd05178d641568aa83353457e954a7ad
Lucas Gomes <lethangomes@gmail.com>
2025-11-01 16:36:37 -0700

    updated lint/prettier settings

--- AI-Textbook-App/eslint.config.js
+++ AI-Textbook-App/eslint.config.js
@@ -1,10 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
+    rules:{
+      "prettier/prettier": [
+        "error",
+        {
+          "printWidth": 100,
+          "tabWidth": 2,
+          "singleQuote": true,
+          "bracketSameLine": true,
+          "endOfLine": "auto"
+        }
+      ]
+    } 
   },
+  
 ]);
