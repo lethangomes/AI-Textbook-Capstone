8e36ef44ed70b49addceceba4f3c97cf598208d3
Lucas Gomes <lethangomes@gmail.com>
2025-11-05 09:55:34 -0800

    fixed linting problem

--- AI-Textbook-App/app/(tabs)/index.tsx
+++ AI-Textbook-App/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { Link } from "expo-router";
+import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
