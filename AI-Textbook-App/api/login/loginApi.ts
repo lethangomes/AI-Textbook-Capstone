ea6be92c2fa748ca22cec26d9b4e12c7056d8b86
Lucas Gomes <lethangomes@gmail.com>
2025-11-15 13:23:19 -0800

    added maestro results file and home.yml takes a screenshot now

--- AI-Textbook-App/api/login/loginApi.ts
+++ AI-Textbook-App/api/login/loginApi.ts
@@ -7,7 +7,8 @@ export async function login(username: string, password: string) {
       return;
     }
 
-    const backendUrl = process.env.EXPO_PUBLIC_API_BASE_URL;
+    const backendUrl = "http://10.0.2.2:8000"//process.env.EXPO_PUBLIC_API_BASE_URL;
+    console.log(backendUrl);
     
 
     const formData = new URLSearchParams();
